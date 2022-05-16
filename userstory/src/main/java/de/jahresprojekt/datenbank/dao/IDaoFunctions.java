9facf7011ab31272e27e5d707d7759160967373d
Blubird <84573489+BlubirdCode@users.noreply.github.com>
2022-05-16 14:45:54 +0200

    alt raus neu rein
    swap

---
+++ userstory/src/main/java/de/jahresprojekt/datenbank/dao/IDaoFunctions.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.jahresprojekt.datenbank.dao;
+
+import java.util.List;
+
+/**
+ *
+ * @author lukas
+ */
+public interface IDaoFunctions<T> {
+  //-------------------------------------------------------------------------
+  //  Method(s)
+  //-------------------------------------------------------------------------     
+    public void save(T item);
+    
+    public T load(int id);
+    
+    public void delete(T item);
+    
+    public List<T> list();
+}
---
+++ userstory/src/main/java/de/jahresprojekt/datenbank/model/ModelTemp.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.jahresprojekt.datenbank.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author lukas
+ */
+public abstract class ModelTemp implements Serializable {
+  //-------------------------------------------------------------------------
+  //  Class-Var(s)
+  //-------------------------------------------------------------------------     
+    protected static long anzahl = 0; 
+
+    public static long getAnzahl() { return anzahl; }
+
+    public static synchronized void incAnzahl() { anzahl++; }
+     
+    
+  //-------------------------------------------------------------------------
+  //  Var(s)
+  //-------------------------------------------------------------------------     
+    protected long id = -1;
+
+
+  //-------------------------------------------------------------------------
+  //  Constructor(s)
+  //-------------------------------------------------------------------------     
+    public ModelTemp()
+    {
+        super();
+        incAnzahl();
+        this.id = anzahl;
+    }
+
+
+  //-------------------------------------------------------------------------
+  //  Get / Set
+  //------------------------------------------------------------------------- 
+    public long getId() { return id; }
+
+    public void setId(long id) { this.id = id; }
+
+    public boolean hasId() { return this.id > -1; }
+
+}
